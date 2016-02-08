c4b3a3ab8ffdce4de3cd719fbcd450d473401dcb
Terry Bu <burea1124@gmail.com>
2016-02-08 11:25:52 -0500

    Initial Commit

---
+++ CenterFloatingActionButtonTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CenterFloatingActionButtonTabBar
+//
+//  Created by Terry Bu on 2/8/16.
+//  Copyright © 2016 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
